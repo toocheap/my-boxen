92a369119d43826e9776748e13778c8508c45d14
Tomoyuki Matsuda(toocheap.jp) <toocheap@gmail.com>
2013-11-12 11:02:11 +0900

    Add Solarized

--- modules/people/manifests/toocheap/repos.pp
+++ modules/people/manifests/toocheap/repos.pp
@@ -14,4 +14,18 @@ class people::toocheap::repos (
     repository { "$SolarizedRepo":
         source => 'altercation/solarized',
     }
+    $iterm2_solarized = "$SolarizedRepo/iterm2-colors-solarized"
+    file { $iterm2_solarized:
+        ensure => directory,
+    }
+    $iterm2_solarized_dark = "$iterm2_solarized/Solarized Dark.itermcolors"
+    file { "$iterm2_solarized_dark":
+        ensure => present,
+        source => $iterm2_solarized_dark,
+    }
+    $iterm2_solarized_light = "$iterm2_solarized/Solarized Light.itermcolors"
+    file { "$iterm2_solarized_light":
+        ensure => present,
+        source => $iterm2_solarized_light,
+    }
 }
